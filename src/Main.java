851343ce0b544e37a2dd43a31b7ec07e7a7c2933
Golyb4ik <only_game2@mail.ru>
2022-11-29 21:41:40 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        //задание 4
-        int deliveryDistance=95;
-        int day=0;
+        //задание 5
+        int monthNumber = 3;
 
-        if(deliveryDistance<20){
-           day= day+1;
-        }
-        else if(deliveryDistance>20 && deliveryDistance<60){
-            day= day+2;
-        }
-        else if(deliveryDistance>60 && deliveryDistance<100) {
-            day = day + 3;
-            System.out.println("Потребуется дней: " + day);
+        switch (monthNumber) {
+            case 1:
+                System.out.println("зима");
+                break;
+            case 2:
+                System.out.println("зима");
+                break;
+            case 3:
+                System.out.println("весна");
+                break;
+            case 4:
+                System.out.println("весна");
+                break;
+            case 5:
+                System.out.println("весна");
+                break;
+            case 6:
+                System.out.println("лето");
+                break;
+            case 7:
+                System.out.println("лето");
+                break;
+            case 8:
+                System.out.println("лето");
+                break;
+            case 9:
+                System.out.println("осень");
+                break;
+            case 10:
+                System.out.println("осень");
+                break;
+            case 11:
+                System.out.println("осень");
+                break;
+            case 12:
+                System.out.println("зима");
+                break;
+            default:
+                System.out.println("Такого нет");
         }
     }
 }
