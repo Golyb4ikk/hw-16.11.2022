aaa0e75ba61bafa828b825f7e0123202add6594f
Golyb4ik <only_game2@mail.ru>
2022-11-29 20:25:09 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
     public static void main(String[] args) {
         //задание 1
         int clientOS=1;
-        if (clientOS==1){
-            System.out.println("Установитеверсию приложения для iOS по ссылке");
-        }
-        else {
+        int clientDeviceYear=1000;
+        if (clientOS==1 && clientDeviceYear>2015 && clientDeviceYear<2022 ) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if(clientOS==1 && clientDeviceYear<2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        else if(clientOS==0 && clientDeviceYear>2015 && clientDeviceYear<2022 ) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        }else{
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+
 
     }
 }
